e59139b4b91393d629a84c9f0165901e31cac441
Trystan Pfluger <code@idlepixel.com>
2014-07-26 23:18:39 +1000

    added example project

---
+++ Example/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  IDLCoachMarksViewExample
+//
+//  Created by Trystan Pfluger on 26/07/2014.
+//  Copyright (c) 2014 Idlepixel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Example/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  IDLCoachMarksViewExample
+//
+//  Created by Trystan Pfluger on 26/07/2014.
+//  Copyright (c) 2014 Idlepixel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
